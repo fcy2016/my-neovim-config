c6e1a1295bda8baf6e9d13d094157853aacd01c7
fcy232005 <fcy232005@163.com>
2024-05-02 16:12:48 +0800

    2024.5.2

--- lua/plugins-config/plugins-config.lua
+++ lua/plugins-config/plugins-config.lua
@@ -1,1 +1,4 @@
 require("plugins-config.cmp")
+require("plugins-config.autopairs")
+require("plugins-config.gopls")
+--require("plugins-config.null-ls")
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,7 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  ft="go",
+  opts=function()
+    return require("plugins-config.null-ls")
+  end
+}
